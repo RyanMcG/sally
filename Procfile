cb6b9e54f0f8517fbdf4ad4eecf10f9c484e6d28
Ryan McGowan <ryan@ryanmcg.com>
2014-09-26 18:16:21 -0700

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/sally.jar clojure.main -m sally.server
---
+++ project.clj
@@ -0,0 +1,48 @@
+(defproject sally "0.1.0-SNAPSHOT"
+  :description "Static analysis library and service"
+  :url "http://sally.clojurecup.com/"
+  :license {:name "MIT"}
+
+  :source-paths ["src/clj" "src/cljs"]
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2342"]
+                 [ring "1.3.1"]
+                 [compojure "1.1.9"]
+                 [enlive "1.1.5"]
+                 [om "0.7.1"]
+                 [figwheel "0.1.4-SNAPSHOT"]
+                 [environ "1.0.0"]
+                 [com.cemerick/piggieback "0.1.3"]
+                 [weasel "0.4.0-SNAPSHOT"]]
+
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-environ "1.0.0"]]
+
+  :min-lein-version "2.0.0"
+
+  :uberjar-name "sally.jar"
+
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+                             :compiler {:output-to     "resources/public/app.js"
+                                        :output-dir    "resources/public/out"
+                                        :source-map    "resources/public/out.js.map"
+                                        :preamble      ["react/react.min.js"]
+                                        :externs       ["react/externs/react.js"]
+                                        :optimizations :none
+                                        :pretty-print  true}}}}
+
+  :profiles {:dev {:repl-options {:init-ns sally.server}
+                   :plugins [[lein-figwheel "0.1.4-SNAPSHOT"]]
+                   :figwheel {:http-server-root "public"
+                              :port 3449 }
+                   :env {:is-dev true}}
+
+             :uberjar {:hooks [leiningen.cljsbuild]
+                       :env {:production true}
+                       :omit-source true
+                       :aot :all
+                       :cljsbuild {:builds {:app
+                                            {:compiler
+                                             {:optimizations :advanced
+                                              :pretty-print false}}}}}})
