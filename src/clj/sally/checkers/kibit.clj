4460bf5537e5800557105c3b6310555ac7a0104e
Ryan McGowan <ryan@ryanmcg.com>
2014-09-27 13:31:16 -0700

    Define kibit checker.

---
+++ src/clj/sally/checkers/kibit.clj
@@ -0,0 +1,26 @@
+(ns sally.checkers.kibit
+  (:require [kibit.check :as kc]
+            [sally.checkers.meta :refer [add-sally-metadata]])
+  (:import [java.io File Reader]
+           [clojure.lang PersistentList]))
+
+(defprotocol KibitCheck
+  (check [thing]))
+
+(extend-protocol KibitCheck
+  Reader
+  (check [r] (kc/check-reader r))
+
+  File
+  (check [f] (kc/check-file f))
+
+  PersistentList
+  (check [expr] (kc/check-expr expr)))
+
+(add-sally-metadata check
+                    :name "kibit"
+                    :description "A static code analyzer for Clojure which uses
+                                 `core.logic` to search for patterns of code for
+                                 which there might exist a more idiomatic
+                                 function or macro."
+                    :source "https://github.com/jonase/kibit")
