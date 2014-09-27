d45836215218bab8481f3d3dc21bc98a8b681a09
Ryan McGowan <ryan@ryanmcg.com>
2014-09-26 20:39:14 -0700

    Add gen-class to directive to main ns.

--- src/clj/sally/main.clj
+++ src/clj/sally/main.clj
@@ -1,5 +1,6 @@
 (ns sally.main
   (:require [sally.server :refer [create-http-server]]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component])
+  (:gen-class))
 
 (defn -main [& _] (component/start (create-http-server)))
