efa30fea96da913f4ff6e27031f769ca53edcf03
Ryan McGowan <ryan@ryanmcg.com>
2014-09-27 18:27:15 -0700

    Fix uberjaring. Require on ns-load.

--- src/clj/sally/checkers/core.clj
+++ src/clj/sally/checkers/core.clj
@@ -2,7 +2,6 @@
   (:require (clojure [string :as s]
                      [test :refer [are with-test]])))
 
-
 (with-test
   (defn- sym-to-var-form [sym]
     (list 'var
@@ -20,15 +19,17 @@
        'hey/there 'hey
        'some.cool.ns/func 'some.cool.ns))
 
-(defmacro defcheckers [& checkers]
-  (let [checker-namespaces (into '() (set (map ns-sym-from-sym checkers)))
-        checker-vars (map sym-to-var-form checkers)]
-    `(defn ~'load-checkers [& {:keys [~'reload] :or {~'reload true}}]
-       (doseq [~'checker-ns '~checker-namespaces]
-         (if ~'reload
-           (require :reload ~'checker-ns)
-           (require ~'checker-ns)))
-       ~(vec checker-vars))))
+(defmacro ^:private defcheckers [& checkers]
+  (let [ns-syms (into '() (set (map ns-sym-from-sym checkers)))
+        checker-var-forms (map sym-to-var-form checkers)]
+    `(do
+       (defn ~'load-checkers [& {:keys [~'reload] :or {~'reload true}}]
+         (doseq [~'checker-ns '~ns-syms]
+           (if ~'reload
+             (require :reload ~'checker-ns)
+             (require ~'checker-ns))))
+       (~'load-checkers :reload true)
+       (def ~'checkers ~(vec checker-var-forms)))))
 
 (defcheckers
   sally.checkers.kibit)
@@ -37,4 +38,4 @@
   ([checkable checkers]
    (into {} (for [checker checkers]
               [checker (checker checkable)])))
-  ([checkable] (check checkable (load-checkers))))
+  ([checkable] (check checkable checkers)))
